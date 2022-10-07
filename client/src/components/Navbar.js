8f67c86066b9684f7df529c76b46fa8c763c4a04
Dharmil Shah <dharmilshah476@gmail.com>
2022-10-08 02:56:49 +0530

    Margin left auto

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -5,7 +5,7 @@ import { NavLink } from "react-router-dom";
 function Navbar() {
   return (
     <>
-      <nav className="navbar navbar-expand-lg navbar-light bg-light">
+      <nav className="navbar navbar-expand-lg navbar-light bg-light px-3">
         <NavLink className="navbar-brand" to="/">
           Navbar
         </NavLink>
