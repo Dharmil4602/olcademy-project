7eae2bcda0972f4cba9587a61db1a95d41b47e18
Dharmil Shah <dharmilshah476@gmail.com>
2022-10-08 02:08:24 +0530

    Added path in navbar

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -23,17 +23,17 @@ function Navbar() {
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
           <ul className="navbar-nav ml-auto">
             <li className="nav-item active">
-              <a className="nav-link" href="#">
+              <a className="nav-link" href="/home">
                 Home
               </a>
             </li>
             <li className="nav-item">
-              <a className="nav-link" href="#">
+              <a className="nav-link" href="/login">
                 Login
               </a>
             </li>
             <li className="nav-item">
-              <a className="nav-link" href="#">
+              <a className="nav-link" href="signup">
                 Signup
               </a>
             </li>
