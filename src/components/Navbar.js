0baa7f31feb8fb13410af193b548570e81e51af7
Dharmil Shah <dharmilshah476@gmail.com>
2022-10-08 18:32:26 +0530

    Changed the font weight in navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -23,17 +23,17 @@ function Navbar() {
 
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
           <ul className="navbar-nav ml-auto">
-            <li className="nav-item active">
+            <li className="nav-item active font-weight-bold">
               <NavLink className="nav-link" to="/">
                 Home
               </NavLink>
             </li>
-            <li className="nav-item">
+            <li className="nav-item active font-weight-bold">
               <NavLink className="nav-link" to="/login">
                 Login
               </NavLink>
             </li>
-            <li className="nav-item">
+            <li className="nav-item active font-weight-bold">
               <NavLink className="nav-link" to="signup">
                 Signup
               </NavLink>
