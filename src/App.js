2c441c17f16560051d890d55da650b63f85c32c3
Dharmil Shah <dharmilshah476@gmail.com>
2022-10-09 01:56:40 +0530

    Deleted login

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import './App.css';
 import Navbar from './components/Navbar';
 import Home from './components/Home';
-import Login from './components/Login';
 import Signup from './components/Signup';
 import { Route, Routes } from 'react-router-dom';
 import Contact from './components/Contact';
@@ -14,7 +13,6 @@ function App() {
       <Routes>
         <Route exact path="/" element={<Signup />}></Route>
         <Route path="/home" element={<Home />}></Route>
-        <Route path="/login" element={<Login />}></Route>
         <Route path="/contact" element={<Contact />}></Route>
       </Routes>
     </>
