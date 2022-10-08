9a1bc0107ea51dd7ad7ae999e8c909cc606f9853
Dharmil Shah <dharmilshah476@gmail.com>
2022-10-08 19:24:05 +0530

    Changed  a bit

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import Home from './components/Home';
 import Login from './components/Login';
 import Signup from './components/Signup';
 import { Route, Routes } from 'react-router-dom';
-import { useState } from 'react';
 
 function App() {
 
